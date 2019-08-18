4e770081cf184ad58bae13190c00cdd5a69385b9
Niall Pereira <niall.pereira@ibm.com>
2019-08-18 19:06:41 -0400

    enhance bookform component

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form v-on:submit.prevent="bookSubmit()" class="" action="#" method="post">
+  <form v-on:submit.prevent="bookSubmit(bookData)" class="" action="#" method="post">
     <input v-model="bookData.bookTitle" type="text" name="title" value="" placeholder="Book Title">
     <input v-model="bookData.bookAuthor" type="text" name="author" value="" placeholder="Book Author">
     <div>
@@ -39,8 +39,8 @@ export default {
     };
   },
   methods: {
-    bookSubmit(bookTitle, bookAuthor) {
-      this.$emit("addBook", bookTitle, bookAuthor);
+    bookSubmit() {
+      this.$emit("addBook", bookData);
     }
   }
 };
